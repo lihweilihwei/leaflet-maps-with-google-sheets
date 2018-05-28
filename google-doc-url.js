c12d3d4a54fcfd22054e7917bfec80c9604dc625
lihweilihwei <lihweilihwei@gmail.com>
2018-05-28 18:14:14 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1jv7qCK4zwbwMig1G_vh09pFZI-ZI1yeaj0FicqlaM6U/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1jv7qCK4zwbwMig1G_vh09pFZI-ZI1yeaj0FicqlaM6U/edit#gid=0';
