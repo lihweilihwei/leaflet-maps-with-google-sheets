f301e743d089d8e4547c4fc2b99a6d26cced5b4b
lihweilihwei <lihweilihwei@gmail.com>
2018-05-29 01:27:01 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1jv7qCK4zwbwMig1G_vh09pFZI-ZI1yeaj0FicqlaM6U/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1zruGKVKrts7Fqu7aSIYeZwKrw23dVh4DyQir08EKCpc/edit#gid=0';
